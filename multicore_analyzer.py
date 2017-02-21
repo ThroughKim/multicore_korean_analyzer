49ccaed4acff9933ea8130bed29a2b67cb73540e
throughKim <sulewoo@naver.com>
2017-02-21 15:06:23 +0900

    add status percentage print

--- multicore_analyzer.py
+++ multicore_analyzer.py
@@ -1,11 +1,12 @@
+from __future__ import division
 import time, os,  operator
 from multiprocessing import Pool
 from konlpy.tag import Kkma
 from collections import Counter
+import sys
 
 
 def parse(file):
-    print("processing file: " + file)
     result_list = []
     kkma = Kkma()
     file = open(folder_name + '/' + file)
@@ -19,11 +20,10 @@ def parse(file):
             type = word_set[1]
             if type in tag_list:
                 if word not in exception_list:
-                    if type == 'VV' or type == 'VA':
-                        word += "다"
                     result_list.append(word)
 
     return result_list
+
 
 def combine_lists(lists):
     combined_list = []
@@ -32,19 +32,28 @@ def combine_lists(lists):
 
     return combined_list
 
+def make_log_result(results, len_file_list):
+    def log_result(retval):
+        results.append(retval)
+        sys.stdout.write(" " + str(round(len(results)/len_file_list*100, 2)) + '% \r')
+
+    return log_result
+
 if __name__ == "__main__":
     start_time = time.time()
     print("분석할 폴더의 이름을 입력하세요.")
     print("현재 파일에 대한 상대경로만 입력하면 되며, 끝에 '/'는 생략해주세요.")
     folder_name = input("폴더이름을 입력하세요:  ")
-    word_count = 50
-    file_count = 0
-    exception_list = ["교수님", "수업", "있", "학생", "강의", "하", "학기", "점", "같", "되", "때"]
-    tag_list = ['NNG', 'NNP', 'VV', 'VA', 'MAG']
+    word_count = 200
+    exception_list = ["교수님", "수업", "학생", "강의", "학기", "점", "때", "시간", "중간", "기말", "고사", "생각", "시간", "동안"]
+    tag_list = ['NNG', 'NNP']
     file_list = os.listdir(folder_name + '/')
 
     pools = Pool(3)
-    results = pools.map(parse, file_list)
+    results = []
+
+    for file in file_list:
+        pools.apply_async(parse, args=[file], callback=make_log_result(results, len(file_list)))
     pools.close()
     pools.join()
 
@@ -59,5 +68,5 @@ if __name__ == "__main__":
     f.close
 
     end_time = time.time()
-    print("완료")
+    print("완료   ")
     print("걸린 시간" + str(end_time - start_time))
