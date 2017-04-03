d2ce7b08f8dce89a77e486209bd02880effbd5da
SeulWoo Kim <sulewoo58@gmail.com>
2017-04-03 14:53:37 +0900

    change wordset of toop and low

--- make_graph.py
+++ make_graph.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import time
 
-G = nx.read_graphml('./top.graphml')
+G = nx.read_graphml('./total.graphml')
 fp1 = fm.FontProperties(fname="./NotoSansCJKkr-Regular.otf", size=8)
 nx.set_fontproperties(fp1)
 
-weights= [G[u][v]['weight'] * 0.002 for u,v in G.edges()]
+weights= [G[u][v]['weight'] * 0.00023 for u,v in G.edges()]
 degree = nx.degree(G)
 
 node_size_by_freq = []
 for n in G.nodes():
     node_freq = G[n][n]['weight']
-    node_size_by_freq.append(node_freq/3)
+    node_size_by_freq.append(node_freq/30)
 
 nx.draw_random(
     G,
