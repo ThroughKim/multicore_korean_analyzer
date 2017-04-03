9a46543d121df1d44bf3dc5bab51676bc3808281
SeulWoo Kim <sulewoo58@gmail.com>
2017-04-03 11:50:30 +0900

    add result graphs

--- make_graph.py
+++ make_graph.py
@@ -3,12 +3,17 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import time
 
-G = nx.read_graphml('./low.graphml')
+G = nx.read_graphml('./top.graphml')
 fp1 = fm.FontProperties(fname="./NotoSansCJKkr-Regular.otf", size=8)
 nx.set_fontproperties(fp1)
 
-weights= [G[u][v]['weight'] * 0.001 for u,v in G.edges()]
+weights= [G[u][v]['weight'] * 0.002 for u,v in G.edges()]
 degree = nx.degree(G)
+
+node_size_by_freq = []
+for n in G.nodes():
+    node_freq = G[n][n]['weight']
+    node_size_by_freq.append(node_freq/3)
 
 nx.draw_random(
     G,
@@ -16,9 +21,8 @@ nx.draw_random(
     node_color = 'yellow',
     edge_color = 'gray',
     width = weights,
-    node_size = [v*v for v in degree.values()]
+    node_size = node_size_by_freq
 )
 end_time = time.time()
 
-# plt.savefig("top_graph.png", dpi=500)
 plt.show()
