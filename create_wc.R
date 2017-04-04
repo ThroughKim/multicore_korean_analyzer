c9670d3952f8f42a9fb7afa25eccd53940e17e42
SeulWoo Kim <sulewoo58@gmail.com>
2017-04-04 16:15:17 +0900

    create R file to make wordcloud

---
+++ create_wc.R
@@ -0,0 +1,10 @@
+library(tm)
+library(wordcloud)
+text_data = read.table("total_output", encoding="UTF-8") 
+text_data
+un_text = table(unlist(text_data))
+un_text
+wc = sort(un_text, decreasing = T)
+pal <- brewer.pal(6, "Dark")
+wordcloud(words = names(wc), freq = wc, colors = pal, min.freq = 5, random.order = F, random.color = T, family="AppleGothic", rot.per = 0, scale=c(7,1))
+
