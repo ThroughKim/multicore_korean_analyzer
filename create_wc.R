df9fca2fd1393a6a4999b777423600c22773c949
SeulWoo Kim <sulewoo58@gmail.com>
2017-04-07 09:37:30 +0900

    intergrate word_extractor

--- create_wc.R
+++ create_wc.R
@@ -1,10 +1,13 @@
+install.packages('tm')
+install.packages('wordcloud')
+
 library(tm)
 library(wordcloud)
 
-text_data = read.table("total_output", encoding="UTF-8") 
+text_data = read.table("low_output", encoding="UTF-8") 
 text_data
 un_text = table(unlist(text_data))
 un_text
 wc = sort(un_text, decreasing = T)
-pal <- brewer.pal(6, "Dark")
+pal <- brewer.pal(6, "Dark2")
 wordcloud(words = names(wc), freq = wc, colors = pal, min.freq = 5, random.order = F, random.color = T, family="AppleGothic", rot.per = 0, scale=c(7,1))
